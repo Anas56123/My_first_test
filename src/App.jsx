71c09ca5297db7f8fea4aacf66a362c1e1c006dd
Anas Ben Messaoud <anasbenmessaoud2000@gmail.com>
2023-12-21 18:01:28 +0100

    now start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./Header";
+import Main from "./MainApp";
+
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return <header>
+    <h1>Profile</h1>
+    <p>Dashboard . User . Jaydon Frankie</p>
+    <img src="./assets/div.MuiPaper-root.png" alt="" />
+    <button>Profile</button>
+    <button>Followers</button>
+    <button>Friends</button>
+    <button>Gallery</button>
+    <aside>
+      <h3>1,947</h3>
+      <p>Follower</p>
+      <h3>1,947</h3>
+      <p>Follower</p>
+    </aside>
+  </header>;
+}
