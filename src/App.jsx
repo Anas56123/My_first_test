5da76c08ea1804b637996e77384bb8f71f3ebd82
Anas Ben Messaoud <anasbenmessaoud2000@gmail.com>
2024-01-14 18:39:58 +0100

    fix emo problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Aside from "./Aside";
 import PostList from "./Func/PostList";
 import Header from "./Header";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-// import LetUsTry from "./LetUsTry";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,14 +12,12 @@ const queryClient = new QueryClient({
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <>
       <Header />
       <div className="split">
         <Aside />
         <PostList />
       </div>
-      {/* <LetUsTry /> */}
-    </QueryClientProvider>
+    </>
   );
 }
